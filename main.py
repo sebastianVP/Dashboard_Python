ff7e32464b20f79b901ed8507b690c8b150ec7d4
sebastianVP <alex.valdezp22@gmail.com>
2025-01-30 17:22:24 -0500

    Biblioteca

---
+++ main.py
@@ -0,0 +1,10 @@
+# importamos las librerias involucradas en el proyeco
+
+import dash_bootstrap_components as doc
+import pandas as pd
+import plotly.express as px
+
+
+from dash import Dash, html,dcc, callback , Output , Input
+
+
